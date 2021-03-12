02fed4e458a928009346a23fb61ad493bfb1a707
Judgement9882 <79979086+Judgement9882@users.noreply.github.com>
2021-03-12 19:47:12 +0900

    BJ 1001
    BJ 1001

---
+++ BJ1001/source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // input output speed considering
+    cin.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    int a, b;
+    cin >> a >> b;
+    cout << a - b;
+
+
+    return 0;
+}
