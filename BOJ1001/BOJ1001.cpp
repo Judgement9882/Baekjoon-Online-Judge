371517ad79ab4f654c534fe5ee5d16ee1b70364e
Judgement9882 <79979086+Judgement9882@users.noreply.github.com>
2021-07-16 11:43:38 +0900

    BOJ1001
    BOJ1001

---
+++ BOJ1001/BOJ1001.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // I/O speed management
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b;
+    cin >> a >> b;
+
+    cout << a + b;
+
+    return 0;
+}
