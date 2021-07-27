3d2fac5c43bf05a15d2e3a79f6899e01a5fcc566
Judgement <79979086+Judgement9882@users.noreply.github.com>
2021-07-27 11:24:08 +0900

    BOJ15894
    BOJ15894

---
+++ BOJ15894.cpp
@@ -0,0 +1,17 @@
+// https://www.acmicpc.net/submit/15894
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+
+	long long N;
+	cin >> N;
+	cout << 4 * N;
+
+	return 0;
+}
