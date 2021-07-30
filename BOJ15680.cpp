6d38729be71b6e43927b5ef8f9f54f94fdc1f9c0
Judgement <79979086+Judgement9882@users.noreply.github.com>
2021-07-30 16:08:34 +0900

    BOJ15680
    BOJ15680

---
+++ BOJ15680.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+	if (N == 0) cout << "YONSEI";
+	else if (N == 1) cout << "Leading the Way to the Future";
+	
+	return 0;
+}
