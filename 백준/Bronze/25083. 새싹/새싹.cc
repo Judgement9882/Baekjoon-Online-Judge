81259665218f6f0c98ea4442379f16180c2bf6e9
Judgement <79979086+Judgement9882@users.noreply.github.com>
2022-05-09 11:20:21 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,33 @@
+/*
+	https://www.acmicpc.net/problem/25083
+
+	새싹 (단순 출력문제)
+
+		 ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+	  |
+	  |
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	//ios::sync_with_stdio(0);
+	//cin.tie(0);
+
+	cout << "         ,r'\"7" << "\n";
+	cout << "r`-_   ,'  ,/" << "\n";
+	cout << " \\. \". L_r'" << "\n";
+	cout << "   `~\\/" << "\n";
+	cout << "      |" << "\n";
+	cout << "      |" << "\n";
+	
+	
+
+	return 0;
+}
