279ba5b8912b12c76cc73decd7c508c3c5ea1730
Judgement <79979086+Judgement9882@users.noreply.github.com>
2022-05-14 09:51:28 +0900

    [Bronze IV] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    long long N, M;
+    
+    cin >> N >> M;
+    if (N==M) cout << "1";
+    else cout << "0";
+        
+    
+    
+    return 0;
+}
