18ed5763eb199f599c6980c0146a9b81969bcc06
Judgement <79979086+Judgement9882@users.noreply.github.com>
2022-05-29 21:52:13 +0900

    [Bronze III] Title: Hello Judge, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,24 @@
+/*
+	https://www.acmicpc.net/problem/9316
+	
+	Hello judge
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cout << "Hello World, Judge " << i << "!\n";
+	}
+
+
+	return 0;
+}
