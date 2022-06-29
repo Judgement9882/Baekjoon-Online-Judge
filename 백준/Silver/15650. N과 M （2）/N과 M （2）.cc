38b9ddbadda4fde6516b5ce8461d7668ffea570a
Judgement <79979086+Judgement9882@users.noreply.github.com>
2022-06-29 20:52:08 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2016 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> v;
+
+bool visit[9];
+
+int index = 1;
+
+void solve(int N, int M, int cnt) {
+    if (cnt == M) {
+        for (int i = 0; i < v.size(); i++) {
+            cout << v[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    for (int i = index; i <= N; i++) {
+
+        if (visit[i]) {
+            continue; // 방문한 곳이면 넘어감
+        }
+
+        visit[i] = 1;
+        v.push_back(i);
+        solve(N, M, cnt + 1);
+        visit[i] = 0;
+        v.pop_back();
+        index = i+1;
+    }
+}
+
+int main() {
+
+    // 입출력 속도 관리
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N, M;
+    cin >> N >> M;
+
+    solve(N, M, 0);
+
+
+    return 0;
+}
