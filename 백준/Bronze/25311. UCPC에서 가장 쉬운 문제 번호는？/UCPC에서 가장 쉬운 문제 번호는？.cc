4e0216cc8179dd2127947b338d3b4a366160e47b
Judgement <79979086+Judgement9882@users.noreply.github.com>
2022-08-09 16:27:42 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.cc
@@ -0,0 +1,20 @@
+/*
+	https://www.acmicpc.net/problem/25311
+
+	가장 쉬운 문제
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+	cout << 'A';
+
+	return 0;
+}
