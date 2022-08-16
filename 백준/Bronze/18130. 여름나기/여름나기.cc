6a23bd2248d4fe0cb8a5a5c486e9e261303741f5
Judgement <79979086+Judgement9882@users.noreply.github.com>
2022-08-16 13:53:22 +0900

    [Bronze II] Title: 여름나기, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18130. 여름나기/여름나기.cc
@@ -0,0 +1,60 @@
+/*
+	https://www.acmicpc.net/problem/18130
+
+	여름나기
+*/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int T, H;
+	cin >> T >> H;
+	
+	long long min_price = 0x7fffffffffffffff;
+	int index = 0;
+
+	for(int i = 1; i <= T; i++){
+
+		long long P, K, C;
+		cin >> P >> K >> C;
+
+		long long temp_per_price = H / K;
+
+		// 나누어 떨어지면 
+		if (H % K == 0) temp_per_price -= 1;
+
+		long long temp_price = P;
+		
+		if (temp_per_price >= 2) {
+			temp_price = P + (temp_per_price * (temp_per_price + 1) / 2) * C;
+		}
+		else {
+			if (temp_per_price == 1) {
+				temp_price = P+C;
+			}
+			else {
+				temp_price = P;
+			}
+		}
+
+		// cout << i << " " << temp_price << "\n";
+		if (min_price > temp_price) {
+			min_price = temp_price;
+			index = i;
+		}
+
+	}
+
+	cout << index << " " << min_price;
+
+	return 0;
+}
+
+
+
