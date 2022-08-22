5a2a31e6f650d099a2e517863034561f84faf4bd
Judgement <79979086+Judgement9882@users.noreply.github.com>
2022-08-22 23:16:30 +0900

    [Bronze V] Title: 余り (Remainder), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.cc
@@ -0,0 +1,19 @@
+/*
+	https://www.acmicpc.net/problem/24078
+
+	余り
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N; cin >> N;
+	cout << N % 21;
+	
+	return 0;
+}
