6844be15575a3bd57da9b0a4369379dba1d80232
Judgement <79979086+Judgement9882@users.noreply.github.com>
2022-08-25 09:53:52 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.cc
@@ -0,0 +1,19 @@
+/*
+	https://www.acmicpc.net/problem/24082
+
+	 (Cube)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int X; cin >> X;
+	cout << X * X * X;
+	
+	return 0;
+}
