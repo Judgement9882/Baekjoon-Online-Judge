6a09b64de8ac98514fce351f45d1ade867dff12e
Judgement <79979086+Judgement9882@users.noreply.github.com>
2022-08-28 20:44:59 +0900

    [Bronze IV] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,23 @@
+/*
+	https://www.acmicpc.net/problem/14581
+
+	 팬 홍준
+*/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	string name; cin >> name;
+	cout << ":fan::fan::fan:\n";
+	cout << ":fan::" << name << "::fan:\n";
+	cout << ":fan::fan::fan:";
+	
+
+	return 0;
+}
