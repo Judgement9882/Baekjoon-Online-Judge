15cb68b435fc65815f6fd78cce884810fb80ba84
Judgement <79979086+Judgement9882@users.noreply.github.com>
2022-09-05 13:26:08 +0900

    [Bronze IV] Title: Can you add this?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,26 @@
+/*
+	https://www.acmicpc.net/problem/7891
+
+	Can you add this?
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N; cin >> N;
+	while (N--) {
+		long long x, y; cin >> x >> y;
+		cout << x + y << "\n";
+		
+	}
+	
+
+
+	return 0;
+}
