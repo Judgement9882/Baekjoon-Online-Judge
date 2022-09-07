45beff1a0485be93240e7fc3b0f350c8cb017c9f
Judgement <79979086+Judgement9882@users.noreply.github.com>
2022-09-07 20:24:48 +0900

    [Bronze IV] Title: SciComLove, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.cc
@@ -0,0 +1,23 @@
+/*
+	https://www.acmicpc.net/problem/21598
+
+	SciComLove
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N; cin >> N;
+	while (N--) {
+		cout << "SciComLove\n";
+	}
+
+
+	return 0;
+}
