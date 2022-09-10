d29b9c7b6c48b76a72832181461a2d73b241fa61
Judgement <79979086+Judgement9882@users.noreply.github.com>
2022-09-10 19:14:02 +0900

    [Bronze V] Title: Cupcake Party, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.cc
@@ -0,0 +1,22 @@
+/*
+	https://www.acmicpc.net/problem/24568
+
+	Cupcake Party
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int A, B; cin >> A >> B;
+	cout << 8 * A + 3 * B - 28;
+
+
+
+	return 0;
+}
