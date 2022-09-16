88153fc132d46bc58c226fc2fd6226ff1f26f266
Judgement <79979086+Judgement9882@users.noreply.github.com>
2022-09-16 10:49:13 +0900

    [Bronze IV] Title: Rectangles, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.cc
@@ -0,0 +1,25 @@
+/*
+	https://www.acmicpc.net/problem/15232
+
+	Rectangles
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int R, C; cin >> R >> C;
+	for (int i = 0; i < R; i++) {
+		for (int j = 0; j < C; j++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	return 0;
+}
