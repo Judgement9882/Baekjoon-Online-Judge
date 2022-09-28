fe2b53ef2c870605aebb08c31f9b036e0d12f310
Judgement <79979086+Judgement9882@users.noreply.github.com>
2022-09-29 00:07:55 +0900

    [Silver II] Title: N과 M (11), Time: 128 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.cc
@@ -0,0 +1,55 @@
+/*
+    https://www.acmicpc.net/problem/15665
+
+    N 과 M 11
+*/
+
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+
+int show_arr[10];
+int arr[10];
+bool isused[10];
+
+void func(int k) {
+    
+    if (M == k) {
+        for (int i = 0; i < M; i++) {
+            cout << show_arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+
+    int temp = 0;
+    for (int i = 0; i < N; i++) {
+        if (temp != arr[i]) {
+            show_arr[k] = arr[i];
+            temp = show_arr[k];
+            func(k + 1);
+        }   
+    }
+}
+
+int main() {
+
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    
+    cin >> N >> M;
+    
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    
+    sort(arr, arr+N);
+    func(0);
+    
+
+    return 0;
+}
