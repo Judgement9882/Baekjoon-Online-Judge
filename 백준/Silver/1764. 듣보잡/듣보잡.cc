41dde362acd6cb3fad209d8660a32ef284ae68de
Judgement <79979086+Judgement9882@users.noreply.github.com>
2022-10-31 01:54:49 +0900

    [Silver IV] Title: 듣보잡, Time: 40 ms, Memory: 9868 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -0,0 +1,67 @@
+#include<iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+
+    vector<string> dud;
+    vector<string> bo;
+
+    int N, M; cin >> N >> M;
+    int temp_N = N, temp_M = M;
+    while (temp_N--) {
+        string temp; cin >> temp; dud.push_back(temp);
+    }
+    while (temp_M--) {
+        string temp; cin >> temp; bo.push_back(temp);
+    }
+
+    sort(dud.begin(), dud.end());
+    sort(bo.begin(), bo.end());
+
+    int it_dud = 0, it_bo = 0;
+    int cnt = 0;
+    vector<string> ans;
+    
+    while (1) {
+        if (it_dud == N || it_bo == M) break;
+
+        if (dud[it_dud] > bo[it_bo]) {
+            if (it_bo != M) {
+                it_bo++;
+            }
+            else {
+                it_dud++;
+            }
+        }
+        else if (dud[it_dud] < bo[it_bo]){
+            if (it_dud != N) {
+                it_dud++;
+            }
+            else {
+                it_bo++;
+            }
+        }
+        else {
+            ans.push_back(dud[it_dud]);
+            if (it_dud != N) {
+                it_dud++;
+            }
+            if (it_bo != N) {
+                it_bo++;
+            }
+            cnt++;
+        }
+    }
+
+    cout << cnt << "\n";
+    for (int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << "\n";
+    }
+
+    return 0;
+}
