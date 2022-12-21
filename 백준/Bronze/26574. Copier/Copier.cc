fab00de28a1c8d45c57ccf94d5e3ef356d71331d
Judgement <79979086+Judgement9882@users.noreply.github.com>
2022-12-21 23:05:49 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int N; cin >> N;
+	while (N--) {
+		int temp;
+		cin >> temp;
+		cout << temp << " " << temp << "\n";
+	}
+
+	return 0;
+}
