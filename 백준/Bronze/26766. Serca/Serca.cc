7ebdcfe1c6b517b102c22a90b3c87c3fb68b0ef1
Judgement <79979086+Judgement9882@users.noreply.github.com>
2023-01-01 16:46:34 +0900

    [Bronze V] Title: Serca, Time: 80 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N; cin >> N;
+	while (N--) {
+        cout << " @@@   @@@ \n";
+        cout << "@   @ @   @\n";
+        cout << "@    @    @\n";
+        cout << "@         @\n";
+        cout << " @       @ \n";
+        cout << "  @     @  \n";
+        cout << "   @   @   \n";
+        cout << "    @ @    \n";
+        cout << "     @     \n";
+	}
+	
+
+	return 0;
+}
+
+ 
