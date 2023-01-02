73384b4856cd25d3b237276d91fe102622c01d62
Judgement <79979086+Judgement9882@users.noreply.github.com>
2023-01-03 01:08:53 +0900

    [Bronze IV] Title: Poziome serca, Time: 144 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26772. Poziome serca/Poziome serca.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N; cin >> N;
+	for (int i = 0; i < N; i++) cout << " @@@   @@@  ";
+	cout << "\n";
+	for (int i = 0; i < N; i++) cout << "@   @ @   @ ";
+	cout << "\n";
+	for (int i = 0; i < N; i++) cout << "@    @    @ ";
+	cout << "\n";
+	for (int i = 0; i < N; i++) cout << "@         @ ";
+	cout << "\n";
+	for (int i = 0; i < N; i++) cout << " @       @  ";
+	cout << "\n";
+	for (int i = 0; i < N; i++) cout << "  @     @   ";
+	cout << "\n";
+	for (int i = 0; i < N; i++) cout << "   @   @    ";
+	cout << "\n";
+	for (int i = 0; i < N; i++) cout << "    @ @     ";
+	cout << "\n";
+	for (int i = 0; i < N; i++) cout << "     @      ";
+	return 0;
+}
