759534476fd5d0182c35f5f588120f686956b184
Judgement <79979086+Judgement9882@users.noreply.github.com>
2023-02-20 22:28:14 +0900

    [Silver I] Title: Z, Time: 92 ms, Memory: 12988 KB -BaekjoonHub

---
+++ 백준/Silver/1074. Z/Z.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+public class Main{
+	static int N, r, c, num, ans;
+	static void cut (int x, int y, int size) {
+		if(size == 1) {
+			if(x == r && y == c){
+				System.out.println(num);
+				System.exit(0);
+			}
+			else num++; 
+			return;
+		}
+		int half = size/2;
+		
+		if(r < x+half && c < y+half) {
+			cut(x, y, half);	
+		}
+		else if (r < x+half && c >= y+half) {
+			num += half*half;
+			cut(x, y+half, half);	
+		}
+		else if (r >= x+half && c < y+half) {
+			num += 2*half*half;
+			cut(x+half, y, half);
+		}
+		else {
+			num += 3*half*half;
+			cut(x+half, y+half, half);			
+		}
+	}
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(),  " ");
+		
+		N = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		num = 0;
+		cut(0, 0, 2 << N);
+		br.close();
+	}
+}
