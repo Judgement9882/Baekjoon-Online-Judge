33f594027c135bc864ed6b820dc034e7afb3e3b1
Judgement <79979086+Judgement9882@users.noreply.github.com>
2023-03-31 10:52:46 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 88 ms, Memory: 11880 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -9,18 +9,50 @@ public class Main {
 		for(int i = 1; i <= N; i++) {
 			A[i] = Integer.parseInt(st.nextToken());
 		}
-		int [] dp = new int[N+1];
-		for(int i = 1; i<= N; i++) {
-			dp[i] = 1;
-			for(int j = 1; j <= i-1; j++) {
-				if(A[j] < A[i] && dp[i] < dp[j]+1) {
-					dp[i] = dp[j]+1;
-				}
+		List<Integer> dp = new ArrayList<>();
+//		int [] dp = new int[N+1];
+		int last = 0;
+		
+		dp.add(A[1]);
+		for(int i = 2; i<= N; i++) {
+			
+			// 이진탐색 풀이
+			// 새로 들어온 값이 dp 배열의 끝보다 작다면
+			// 이진탐색을 통해 들어갈 수 있는 자리에 넣음
+			if(A[i] > dp.get(last)) { // dp에 마지막에 저장된 수보다 큰 경우
+				// 갱신하지 않고 dp에 추가함
+				last++;
+				dp.add(A[i]);
 			}
+			else { // 저장된 수보다 작거나 같을경우 이진탐색
+				int idx = Collections.binarySearch(dp, A[i]);
+//				System.out.println("idx "+idx);
+				if(idx < 0) dp.set(-(idx+1), A[i]);
+			}
+		
+//			for(int j : dp) {
+//				System.out.print(j + " ");
+//			}
+//			System.out.println();
+		
+//		for(int i = 1; i<= N; i++) {
+//			
+//			// 이진탐색 풀이
+//			// 새로 들어온 값이 dp 배열의 끝보다 작다면
+//			// 이진탐색을 통해 들어갈 수 있는 자리에 넣음
+//
+//			if(A[i] > dp[last]) { // dp에 마지막에 저장된 수보다 큰 경우
+//				// 갱신하지 않고 dp에 추가함
+//				dp[++last] = A[i];
+//			}
+//			else { // 저장된 수보다 작거나 같을경우 이진탐색
+//				int idx = binary
+//				
+//				
+//				dp[idx] = A[i];
+//			}
 		}
-		int max_num = 0;
-		for(int lis : dp) max_num = Math.max(max_num, lis);
-		System.out.println(max_num);
+		System.out.println(dp.size());
 		br.close();
 	}
 }
