624ba1c271d971d9f8d6d4dafe4f795492caa689
Judgement <79979086+Judgement9882@users.noreply.github.com>
2023-04-03 17:21:43 +0900

    [Gold V] Title: 톱니바퀴, Time: 76 ms, Memory: 11788 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -1,87 +1,104 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
-	static ArrayList<Integer>[] topni;
-	static int[] canRotate;
-	static int K, ans;
-	
-	static void solve() {
-		for(int i = 0;i < 4; i++) {
-			if(topni[i].get(0) == 1) {
-				ans += (1 << i);
-			}
-		}
-	}
-
-	static void Rotate() {
-		for (int i = 0; i < 4; i++) {
-			int dir = canRotate[i];
-
-			// 시계방향
-			if(dir == 1) {
-				int temp = topni[i].get(7);
-				topni[i].add(0, temp);
-				topni[i].remove(8);
-			}
-			
-			// 반시계방향
-			else if (dir == -1) {
-				int temp = topni[i].get(0);
-				topni[i].add(temp);
-				topni[i].remove(0);
-			}
-		}
-	}
-
-	static void checkCanRotate(int num, int dir) {
-		canRotate[num] = dir;
-
-		// 왼쪽 확인
-		for (int i = num - 1; i >= 0; i--) {
-			// 왼쪽의 2번과 오른쪽의 6번이 다르다면?
-			if (topni[i].get(2) != topni[i + 1].get(6)) {
-				canRotate[i] = canRotate[i + 1] * (-1);
-			} else
-				break;
-		}
-
-		// 오른쪽 확인
-		for (int i = num + 1; i < 4; i++) {
-			// 왼쪽의 2번과 오른쪽의 6번이 다르다면?
-			if (topni[i - 1].get(2) != topni[i].get(6)) {
-				canRotate[i] = canRotate[i - 1] * (-1);
-			} else
-				break;
-		}
-	}
-
-	public static void main(String[] args) throws Exception {
+	static int N;
+	static List<Integer>[] magnet;
+	static boolean [] connection;
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		topni = new ArrayList[4];
-		for (int i = 0; i < 4; i++) {
-			topni[i] = new ArrayList<>();
-
+		StringBuilder sb = new StringBuilder();
+		
+		magnet = new List[4];
+		// 입력
+		for(int k = 0 ; k < 4; k++) {
+			magnet[k] = new ArrayList<>();
 			String temp = br.readLine();
-			for (int j = 0; j < 8; j++) {
-				topni[i].add(temp.charAt(j) - '0');
+			for(int i = 0 ; i < 8; i++) {
+				magnet[k].add(temp.charAt(i) - '0');	
 			}
 		}
-
-		K = Integer.parseInt(br.readLine());
-		for (int i = 0; i < K; i++) {
+		N = Integer.parseInt(br.readLine());
+		for(int order = 0 ; order < N; order++) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int num = Integer.parseInt(st.nextToken()) - 1;
+			int num = Integer.parseInt(st.nextToken())-1; // 0 ~ 3
 			int dir = Integer.parseInt(st.nextToken());
-			canRotate = new int[4];
-
-			checkCanRotate(num, dir);
-			Rotate();
+			
+			// 1. 현재 연결 상태 확인 (0-1, 1-2, 2-3)
+			connection = new boolean[3];
+			checkConnection();
+			
+			// 2. 회전 (회전함수 구현)
+			if(num == 0) {
+				rotate(num, dir);
+				for(int i = 0; i < 3; i++) {
+					if(connection[i]) {
+						dir *= -1;
+						rotate(i+1, dir);
+					}
+					else break; // 하나라도 안된다면 바로 break
+				}
+			}
+			else if (num == 3) {
+				rotate(num, dir);
+				for(int i = 2; i >= 0; i--) {
+					if(connection[i]) {
+						dir *= -1;
+						rotate(i, dir);
+					}
+					else break; // 하나라도 안된다면 바로 break
+				}
+			}
+			else if (num == 1) {
+				rotate(num, dir);
+				if(connection[0]) rotate(0, dir*-1);
+				for(int i = 1; i < 3; i++) {
+					if(connection[i]) {
+						dir *= -1;
+						rotate(i+1, dir);
+					}
+					else break; // 하나라도 안된다면 바로 break
+				}
+			}
+			else if (num == 2) {
+				rotate(num, dir);
+				if(connection[2]) rotate(3, dir*-1);
+				for(int i = 1; i >= 0; i--) {
+					if(connection[i]) {
+						dir *= -1;
+						rotate(i, dir);
+					}
+					else break; // 하나라도 안된다면 바로 break
+				}
+			}
 		}
-		ans = 0;
-		solve();
+		
+		// 3. 점수 측정 
+		int ans = 0;
+		for(int n = 0; n < 4; n++) {
+			ans += magnet[n].get(0)*(1<<n);
+		}
+			
 		System.out.println(ans);
 		br.close();
+	}
+	
+	static void checkConnection() {
+		// 0-1, 1-2, 2-3
+		if(magnet[0].get(2) != magnet[1].get(6)) connection[0] = true;
+		if(magnet[1].get(2) != magnet[2].get(6)) connection[1] = true;
+		if(magnet[2].get(2) != magnet[3].get(6)) connection[2] = true;
+	}
+	
+	static void rotate(int num, int dir) {
+		if(dir == -1) { // 반시계
+			int temp = magnet[num].get(0);
+			magnet[num].remove(0);
+			magnet[num].add(temp);
+		}
+		else { // 시계
+			int temp = magnet[num].get(7);
+			magnet[num].remove(7);
+			magnet[num].add(0, temp);
+		}
 	}
 }
