da51eefa242951e2e44c6f0fbc49cefbaed2ab2e
Judgement <79979086+Judgement9882@users.noreply.github.com>
2023-04-03 14:48:50 +0900

    [Silver II] Title: 친구, Time: 120 ms, Memory: 13544 KB -BaekjoonHub

--- 백준/Silver/1058. 친구/친구.java
+++ 백준/Silver/1058. 친구/친구.java
@@ -1,44 +1,43 @@
 import java.io.*;
-
+import java.util.*;
 public class Main {
-
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
 		int N = Integer.parseInt(br.readLine());
-		boolean[][] friends = new boolean[N][N];
-		boolean[][] dp = new boolean[N][N];
-		for(int i=0; i<N; i++) {
-			String line = br.readLine();
-			for(int j=0; j<N; j++) {
-				if(line.charAt(j) == 'Y') {
-					dp[i][j] = true;
-					friends[i][j] = true;
-				}
-				else {
-					dp[i][j] = false;
-					friends[i][j] = false;
-				}
+		int [][] board = new int[N][N];
+		for(int i = 0 ; i < N; i++) {
+			String temp = br.readLine();
+			for(int j = 0 ; j < temp.length(); j++) {
+				board[i][j] = temp.charAt(j)=='N'?0:1;
 			}
 		}
-
-		int ans = 0;
-		for(int k=0; k<N; k++) {
-			for(int i=0; i<N; i++) {
-				for(int j=0; j<N; j++) {
-					if(i==j) continue;
-					if(friends[i][k] && friends[k][j]) dp[i][j] = true;
+		HashSet<Integer>[] set = new HashSet[N];
+		for(int i = 0 ; i < N; i++)set[i] = new HashSet<>();
+		
+		if(N==2) {
+			if(board[0][1]==1 && board[1][0] == 1) {
+				System.out.println(1);
+			}else System.out.println(0);
+		}
+		else {
+			for(int k = 0 ; k < N; k++) {
+				for(int i = 0; i < N; i++) {
+					if(i==k) continue;
+					for(int j = 0; j < N; j++) {
+						if(k==j || j == i) continue;
+						if((board[i][k] == 1 && board[k][j] == 1) || board[i][j] == 1) {
+							set[i].add(j);
+						}
+					}
 				}
 			}
-		}
-
-		for(int i=0; i<N; i++) {
-			int cnt = 0;
-			for(int j=0; j<N; j++) {
-				if(dp[i][j]) cnt++;
+			
+			int max = 0;
+			for(int i = 0 ; i < N; i++) {
+				max = Math.max(set[i].size(), max);
 			}
-			ans = Math.max(ans, cnt);
+			System.out.println(max);
 		}
-		System.out.println(ans);
+		br.close();
 	}
 }
