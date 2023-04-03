0083f16ee1beab3b0f9e792d551e4a0bb4ebacc3
Judgement <79979086+Judgement9882@users.noreply.github.com>
2023-04-03 15:30:04 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 108 ms, Memory: 12640 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -1,70 +1,69 @@
 import java.io.*;
 import java.util.*;
+
+
 public class Main {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		test : for(int tc = 0; tc < T; tc++) {
-			// n
-			int n = Integer.parseInt(br.readLine());
-			
-			// home
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			int [] home = new int[] {x, y};
-			
-			// shop
-			ArrayList<int[]> shop = new ArrayList<>();
-			for(int i = 0 ; i < n; i++) {
-				st = new StringTokenizer(br.readLine(), " ");
-				x = Integer.parseInt(st.nextToken());
-				y = Integer.parseInt(st.nextToken());
-				shop.add(new int[] {x, y});
-			}
-			
-			// festival
-			st = new StringTokenizer(br.readLine(), " ");
-			x = Integer.parseInt(st.nextToken());
-			y = Integer.parseInt(st.nextToken());
-			int [] fest = new int[] {x, y};
-			
-			// 만약 집에서 페스티벌까지 바로 갈 수 있다면
-			if(calcDist(home[0], fest[0], home[1], fest[1]) <= 1000) {
-				sb.append("happy\n");
-			}else {
-				ArrayDeque<int[]> q = new ArrayDeque<>();
-				q.offer(new int[] {home[0], home[1]});
-				
-				while(!q.isEmpty()) {
-					int cur[] = q.poll();
-//					System.out.println(cur[0]);
-					for(int i = shop.size()-1 ; i >= 0; i--) {
-						int temp[] = shop.get(i);
-						// 현재 위치에서 갈 수 있다면
-						if(calcDist(cur[0], temp[0], cur[1], temp[1]) <= 1000) {  
-							// 그 위치가 페스티벌 까지 갈 수 있다면
-							if(calcDist(fest[0], temp[0], fest[1], temp[1]) <= 1000) {
-								sb.append("happy\n");
-								continue test;
-							}
-							// 갈수 없다면 큐에 넣고 돌림
-							q.offer(new int[] {temp[0], temp[1]});
-							
-							// 방문 처리를 리스트에서 제거하는 형식으로함
-							shop.remove(i);
-						}
-					}
-				}
-				sb.append("sad\n");
+	
+	static int N;
+	static List<int[]> cpos;
+	static int hy, hx, fy, fx;
+	
+	private static String solution() {
+		ArrayDeque<int[]> q = new ArrayDeque<>();
+		boolean[] visited = new boolean[N];
+		q.offer(new int[] {hy, hx});
+		
+		while(!q.isEmpty()) {
+			int y = q.peek()[0];
+			int x = q.peek()[1];
+			q.poll();
+
+			int remain = Math.abs(y-fy) + Math.abs(x-fx);
+			if(remain <= 1000) return "happy";
+			for(int i=0; i<N; i++) {
+				int cy = cpos.get(i)[0];
+				int cx = cpos.get(i)[1];
+				int dist = Math.abs(y-cy) + Math.abs(x-cx);
+				if(visited[i] || dist>1000) continue;
+				q.offer(new int[] {cy, cx});
+				visited[i] = true;
 			}
 		}
-		System.out.println(sb);
-		br.close();
+		return "sad";
 	}
-	
-	static int calcDist(int x, int y, int i, int j) {
-		return Math.abs(x-y)+Math.abs(i-j);
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = null;
+		
+		int T = Integer.parseInt(br.readLine());
+		for(int t=1; t<=T; t++) {
+			
+			N = Integer.parseInt(br.readLine());
+			
+			// 상근이네 집
+			st = new StringTokenizer(br.readLine());
+			hy = Integer.parseInt(st.nextToken());	
+			hx = Integer.parseInt(st.nextToken());	
+			
+			// 편의점
+			cpos = new ArrayList<>();
+			for(int i=0; i<N; i++) {
+				st = new StringTokenizer(br.readLine());
+				cpos.add(new int[] {Integer.parseInt(st.nextToken()), 
+						Integer.parseInt(st.nextToken())
+						});
+			}
+			
+			// 펜타포트 락 페스티벌
+			st = new StringTokenizer(br.readLine());
+			fy = Integer.parseInt(st.nextToken());	
+			fx = Integer.parseInt(st.nextToken());	
+			sb.append(solution() + "\n");
+		}
+		
+		System.out.println(sb.toString());
+		br.close();
 	}
 }
