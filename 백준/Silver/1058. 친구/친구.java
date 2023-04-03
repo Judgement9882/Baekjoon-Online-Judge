4576e5a441d0a06b340b3ccaa889816105f09947
Judgement <79979086+Judgement9882@users.noreply.github.com>
2023-04-03 14:54:03 +0900

    [Silver II] Title: 친구, Time: 92 ms, Memory: 12016 KB -BaekjoonHub

--- 백준/Silver/1058. 친구/친구.java
+++ 백준/Silver/1058. 친구/친구.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 public class Main {
 	public static void main(String[] args) throws Exception{
+		final int INF = Integer.MAX_VALUE/2;
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int [][] board = new int[N][N];
@@ -9,35 +10,29 @@ public class Main {
 			String temp = br.readLine();
 			for(int j = 0 ; j < temp.length(); j++) {
 				board[i][j] = temp.charAt(j)=='N'?0:1;
+				if(board[i][j] == 0 && i!=j) board[i][j] =INF; 
 			}
 		}
-		HashSet<Integer>[] set = new HashSet[N];
-		for(int i = 0 ; i < N; i++)set[i] = new HashSet<>();
-		
-		if(N==2) {
-			if(board[0][1]==1 && board[1][0] == 1) {
-				System.out.println(1);
-			}else System.out.println(0);
-		}
-		else {
-			for(int k = 0 ; k < N; k++) {
-				for(int i = 0; i < N; i++) {
-					if(i==k) continue;
-					for(int j = 0; j < N; j++) {
-						if(k==j || j == i) continue;
-						if((board[i][k] == 1 && board[k][j] == 1) || board[i][j] == 1) {
-							set[i].add(j);
-						}
-					}
+		for(int k = 0 ; k < N; k++) {
+			for(int i = 0; i < N; i++) {
+				if(i==k) continue;
+				for(int j = 0; j < N; j++) {
+					if(k==j || j == i) continue;
+					board[i][j] = Math.min(board[i][j], board[i][k]+board[k][j]);
 				}
 			}
-			
-			int max = 0;
-			for(int i = 0 ; i < N; i++) {
-				max = Math.max(set[i].size(), max);
-			}
-			System.out.println(max);
 		}
+		
+		int max = 0;
+		for(int i = 0 ; i < N; i++) {
+			int cnt = 0;
+			for(int j = 0 ; j < N; j++) {
+				if(i==j) continue;
+				if(board[i][j] <= 2) cnt++;
+			}
+			max = Math.max(cnt, max);
+		}
+		System.out.println(max);
 		br.close();
 	}
 }
