e58e3a980c2e06b75a2698dda9908066aae46048
Judgement <79979086+Judgement9882@users.noreply.github.com>
2023-04-05 11:01:24 +0900

    [Gold IV] Title: 키 순서, Time: 480 ms, Memory: 35680 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -1,50 +1,42 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
-	static int N, M;
-	static final int INF = Integer.MAX_VALUE/2;
-	static int [][] g;
-	public static void main(String[] args) throws Exception {
+	static int N, M, adj[][];
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st =new StringTokenizer(br.readLine(), " ");
-		StringBuilder sb = new StringBuilder();
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		g = new int[N+1][N+1];
-		for(int i = 1 ; i <= N; i++) {
-			for(int j = 1 ; j <= N; j++) {
-				if(i!=j) g[i][j] = INF;
-			}
+		adj=new int[N+1][N+1]; // 자신보다 키가 큰 관계 표현
+		for(int m = 0; m < M; m++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			adj[a][b] = 1; // 유향
 		}
 		
-		for(int i = 0 ; i < M; i++) {
-			st =new StringTokenizer(br.readLine(), " ");
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			g[from][to] = 1;
-		}
-		for(int k = 1; k <= N; k++) {
-			for(int i = 1; i <= N; i++) {
-				if(i==k) continue;
-				for(int j = 1 ; j <= N; j++) {
-					if(k==j || i==j) continue;
-					g[i][j] = Math.min(g[i][k]+g[k][j], g[i][j]);
+		for(int k = 1; k <= N; k++) { // 경
+			for(int i = 1; i <= N; i++) { // 출
+				if(i==k || adj[i][k] == 0) continue; // 같거나 i -> k 관계 없으면 노필요
+				for(int j = 1; j <= N; j++) { // 도
+					if(adj[i][j]==1) continue; // 이미 관계를 알면 필요없음
+					adj[i][j] = adj[k][j];
 				}
 			}
 		}
 		
-		int [] ans = new int[N+1];
-		int cnt = 0;
-		for(int k = 1; k <= N; k++) {
-			for(int i = 1; i <= N; i++) {
-				if(k==i) continue;
-				if(g[k][i] != INF) ans[k]++;
-				if(g[i][k] != INF) ans[k]++;
+		int ans = 0;
+		for(int i = 1; i <= N; i++) {
+			for(int j = 1; j <= N; j++) {
+				adj[i][0] += adj[i][j]; // 자신보다 키가 큰 학생수 누적
+				adj[0][j] += adj[i][j]; // 자신보다 키가 작은 학생수 누적
 			}
-			if(ans[k] == N-1) cnt++;
 		}
-		System.out.println(cnt);
+		
+		for(int i = 1; i <= N; i++) {
+			if(adj[i][0] + adj[0][i] == N-1) ans++;
+		}
+		System.out.println(ans);
 		br.close();
 	}
 }
