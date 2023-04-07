b2dbf79a9f54201cc1ef1e0774fbe8fbf36b2cd3
Judgement <79979086+Judgement9882@users.noreply.github.com>
2023-04-07 15:13:02 +0900

    [Gold I] Title: 다리 만들기 2, Time: 80 ms, Memory: 11644 KB -BaekjoonHub

--- 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
+++ 백준/Gold/17472. 다리 만들기 2/다리 만들기 2.java
@@ -1,166 +1,128 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-	static int N, M, contCnt, ans;
-	static int[][] board;
-	static int[] dx = { 0, 1, 0, -1 }, dy = { 1, 0, -1, 0 };
-	static int[] p; // 대륙의 부모를 저장하는 배열
-	static ArrayDeque<int[]> q = new ArrayDeque<>();
-	static PriorityQueue<Edge> edges;
-	
-	static class Edge implements Comparable<Edge>{
-		int from, to, dist;
-		public Edge(int from, int to, int dist) {
-			this.from = from;
-			this.to = to;
-			this.dist = dist;
-		}
-
-		@Override
-		public int compareTo(Edge o) {
-			return Integer.compare(this.dist, o.dist);
-		}
-	}
-	
-	static void makeSet(int size) {
-		p = new int[size + 1];
-		for (int i = 0; i <= size; i++)
-			p[i] = i;
-	}
-
-	static int findSet(int a) {
-		if (a == p[a])
-			return a;
-		return p[a] = findSet(p[a]);
-	}
-
-	static boolean union(int a, int b) {
-		int aRoot = findSet(a);
-		int bRoot = findSet(b);
-		if (aRoot == bRoot)
-			return false;
-		p[bRoot] = aRoot;
-		return true;
-	}
-
-	// 낮은 거리 순으로 체크
-	static void solve() {
-		// 1. p 초기화
-		makeSet(contCnt);
-		
-		// 2. 정렬 -> pq는 이미 되어있음
-		// 3. 다리 길이 구하기
-		
-		int conn = 0;
-		int temp_ans = 0;
-		while(!edges.isEmpty()) {
-			Edge cur = edges.poll();
-			// 만약 union 가능하면 진행
-			if(union(cur.from, cur.to)) {
-				temp_ans += cur.dist;
-				if(++conn == contCnt - 1) {
-					ans = Math.min(ans, temp_ans);
-					break;
-				}
-			}
-		}
-	}
-	
-	// 출발 - 도착 거리 구하기
-	static void dist(int x, int y, int color) {
-		for(int d = 0; d < 4 ; d++) {
-			int nx = x;
-			int ny = y;
-			int cnt = 0;
-			while(true) {
-				nx += dx[d];
-				ny += dy[d];
-
-				if (nx < 0 || nx >= N || ny < 0 || ny >= M)
-					break;
-				// 같은 색이 나오면 추가하지 않음
-				if(board[nx][ny] == color)
-					break;
-				
-				// 다른 색이 나오면 그게 최솟값.
-				if(board[nx][ny] != 0) {
-					// 만약 거리가 2 미만이면 추가안함
-					if(cnt < 2) break;
-					
-					// 출발, 도착, 거리
-					edges.offer(new Edge(color, board[nx][ny], cnt));
-					break;
-				}
-				
-				// 바다면 다리 길이 늘림
-				cnt++;
-			}
-		}
-	}
-
-	static void bfs(int x, int y, int color) {
-		board[x][y] = color;
-		q.offer(new int[] { x, y });
-		while (!q.isEmpty()) {
-			int cur[] = q.poll();
-			for (int dir = 0; dir < 4; dir++) {
-				int nx = cur[0] + dx[dir];
-				int ny = cur[1] + dy[dir];
-
-				if (nx < 0 || nx >= N || ny < 0 || ny >= M)
-					continue;
-				if (board[nx][ny] != -1)
-					continue;
-
-				board[nx][ny] = color;
-				q.offer(new int[] { nx, ny });
-			}
-		}
-	}
-
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-
-		board = new int[N][M];
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			for (int j = 0; j < M; j++) {
-				// 육지는 -1
-				board[i][j] = -Integer.parseInt(st.nextToken());
-			}
-		}
-
-		// 같은 영역 확인
-		int area = 1;
-		contCnt = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (board[i][j] == -1) {
-					bfs(i, j, area++);
-					contCnt++;
-				}
-			}
-		}
-		
-		// 거리 구하기
-		edges = new PriorityQueue<>();
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (board[i][j] != 0) {
-					dist(i, j, board[i][j]);
-				}
-			}
-		}
-		
-
-		ans = Integer.MAX_VALUE;
-		solve();
-		System.out.println(ans==Integer.MAX_VALUE?-1:ans);
-		br.close();
-	}
+public class Main {
+    static int[][] map, link;
+    static int[] di= {-1,0,1,0}, dj= {0,1,0,-1}; //상우하좌
+    static int N, M;
+    static boolean[][] v;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        for(int i=0;i<N;i++) {
+            st = new StringTokenizer(br.readLine()," ");
+            for(int j=0;j<M;j++) map[i][j] = Integer.parseInt(st.nextToken());
+        }
+        
+        v = new boolean[N][M];
+        int num = 1;
+        for(int i=0;i<N;i++) {
+            for(int j=0;j<M;j++) {
+                if(map[i][j]==1 && !v[i][j]) {
+                    bfs(i,j,num);
+                    num++;
+                }
+            }
+        }
+        link = new int[num-1][num-1]; //섬 갯수만큼 배열 만들어서
+        for(int i=0;i<N;i++)
+            available_link(map[i], M);
+        for(int i=0;i<M;i++) {
+            int[] arr = new int[N];
+            for(int j=0;j<N;j++) arr[j] = map[j][i];
+            available_link(arr, N);
+        } //각 행, 열마다 만들 수 있는 다리 체크해서 다리 길이 link 배열에 넣기
+        
+        //Prim 알고리즘
+        boolean[] vertex = new boolean[num-1];
+        int[] minEdge = new int[num-1];
+        for(int i=0;i<num-1;i++) //{
+//            System.out.println(Arrays.toString(link[i]));
+            minEdge[i] = Integer.MAX_VALUE;
+//        }
+//        System.out.println();
+        int result = 0, cnt = 0;
+        minEdge[0]=0;
+        for(int i=0;i<num-1;i++) {
+            int minVertex = -1;
+            int min = Integer.MAX_VALUE;
+            for(int j=0;j<num-1;j++) {
+                if(!vertex[j] && min>minEdge[j]) {
+                    minVertex=j;
+                    min = minEdge[j];
+                }
+            }
+            if(minVertex==-1) break;
+            vertex[minVertex] = true;
+            if(cnt++ == num-2) break;
+            for(int j=0;j<num-1;j++) {
+                if(!vertex[j] && link[minVertex][j]!=0 && minEdge[j]>link[minVertex][j])
+                    minEdge[j]=link[minVertex][j];
+            }
+//            System.out.println(Arrays.toString(minEdge));
+        }
+        for(int i=1;i<num-1;i++) {
+            if(minEdge[i]==Integer.MAX_VALUE) { //하나라도 MAX값이면 -1저장하고 break
+                result=-1;
+                break;
+            }
+            result+=minEdge[i]; //다리 길이 합 저장
+        }
+        System.out.println(result);
+        
+        br.close();
+    }
+    static void bfs(int x, int y, int n) {
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        q.offer(new int[] {x,y});
+        v[x][y]=true;
+        map[x][y]=n;
+        while(!q.isEmpty()) {
+            int[] ij = q.poll();
+            for(int d=0;d<4;d++) {
+                int ni = ij[0]+di[d];
+                int nj = ij[1]+dj[d];
+                if(ni>=0 && nj>=0 && ni<N && nj<M && !v[ni][nj] && map[ni][nj]==1){
+                    v[ni][nj]=true;
+                    map[ni][nj]=n;
+                    q.offer(new int[] {ni,nj});
+                }
+            }
+        }
+    }
+    static void available_link(int[] arr, int size) {
+        int start = 0, end = 0;
+        while(true) {
+            end++;
+            if(end>=size) break;
+            if(arr[start]==arr[end]) continue;
+            
+            if(arr[start]==0) start=end;
+            else {
+                start=end;
+                while(true) {
+                    end++;
+                    if(end>=size || arr[start]!=arr[end]) break;
+                }
+                start--;
+            }
+            if(end>=size) break;
+            
+            int a = arr[start];
+            int b = arr[end];
+            if(a>0 && b>0 && a!=b && end-start-1>1) {
+                if(link[a-1][b-1]==0) {
+                    link[a-1][b-1]=end-start-1;
+                    link[b-1][a-1]=end-start-1;
+                } else {
+                    link[a-1][b-1]=Math.min(link[a-1][b-1], end-start-1);
+                    link[b-1][a-1]=Math.min(link[b-1][a-1], end-start-1);
+                }
+            }
+            start=end;
+        }
+    }
 }
