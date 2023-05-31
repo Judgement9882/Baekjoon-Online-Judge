776f20a0577c8217df716054fa4a25f14b3df568
Judgement <79979086+Judgement9882@users.noreply.github.com>
2023-05-31 10:00:09 +0900

    [Bronze III] Title: 캥거루 세마리, Time: 80 ms, Memory: 11536 KB -BaekjoonHub

---
+++ 백준/Bronze/2965. 캥거루 세마리/캥거루 세마리.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+		int diff = Math.max(b-a, c-b);
+		System.out.println(diff-1);
+		
+//		System.out.println(sb);
+		br.close();
+	}
+}
