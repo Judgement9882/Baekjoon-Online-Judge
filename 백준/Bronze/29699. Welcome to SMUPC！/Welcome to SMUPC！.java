bac3e0171af1cd9c6812cb89a7c6f6c6766157d2
Judgement <79979086+Judgement9882@users.noreply.github.com>
2023-09-10 14:43:57 +0900

    [Bronze V] Title: Welcome to SMUPC!, Time: 80 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Bronze/29699. Welcome to SMUPC！/Welcome to SMUPC！.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String label = "WelcomeToSMUPC";
+        int N = Integer.parseInt(br.readLine())-1;
+        System.out.print(label.charAt(N%label.length()));
+        br.close();
+    }
+}
