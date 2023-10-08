0f2e36f5a5fd296e8a4c059360cd08ec40e252d5
Judgement <79979086+Judgement9882@users.noreply.github.com>
2023-10-08 12:58:35 +0900

    [Silver IV] Title: Best Grass, Time: 84 ms, Memory: 11920 KB -BaekjoonHub

---
+++ 백준/Silver/6186. Best Grass/Best Grass.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static int [] dx = {0, 1, 0, -1}, dy = {1, 0, -1, 0};
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        char board[][] =  new char[R][C];
+        for(int i = 0 ; i < R; i++){
+            String temp = br.readLine();
+            for(int j = 0; j < C; j++){
+                board[i][j] = temp.charAt(j);
+            }
+        }
+
+        int ans = 0;
+        for(int i = 0 ; i < R; i++){
+            for(int j = 0 ; j < C; j++){
+                if(board[i][j] == '#'){
+                    ans++;
+                    // bfs
+                    ArrayDeque<int[]> q = new ArrayDeque<>();
+                    board[i][j] = '.';
+                    q.offer(new int[] {i, j});
+                    while(!q.isEmpty()){
+                        int cur [] = q.poll();
+                        for(int dir = 0; dir < 4 ; dir++){
+                            int nx = dx[dir] + cur[0];
+                            int ny = dy[dir] + cur[1];
+
+                            // 범위
+                            if(nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
+                            if(board[nx][ny] == '.') continue;
+
+                            board[nx][ny] = '.';
+                            q.offer(new int[] {nx, ny});
+                        }
+                    }
+                }
+            }
+        }
+        System.out.print(ans);
+        br.close();
+    }
+}
