81b3fd9d561003943cf83823e42a66d5c91cec0e
Judgement <79979086+Judgement9882@users.noreply.github.com>
2024-02-05 08:36:49 +0900

    [Gold I] Title: 수열과 쿼리 16, Time: 876 ms, Memory: 110552 KB -BaekjoonHub

---
+++ 백준/Gold/14428. 수열과 쿼리 16/수열과 쿼리 16.java
@@ -0,0 +1,105 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+    static class Node {
+        long val;
+        int idx;
+        Node(long val, int idx){
+            this.val = val;
+            this.idx = idx;
+        }
+    }
+    static long arr [];
+    static Node segTree[];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        arr = new long[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) arr[i] = Long.parseLong(st.nextToken());
+
+        int H = (int)Math.ceil(Math.log(N)/Math.log(2));
+        int tree_size = (1<<(H+1));
+        segTree = new Node[tree_size];
+        init(1, 0, N-1);
+
+        int M = Integer.parseInt(br.readLine());
+        while(M-->0){
+            st = new StringTokenizer(br.readLine());
+            int order = Integer.parseInt(st.nextToken());
+            if(order==2) {
+                int i = Integer.parseInt(st.nextToken());
+                int j = Integer.parseInt(st.nextToken());
+                sb.append(query(1, 0, N-1, i-1, j-1).idx).append("\n");
+            }
+            else{
+                int i = Integer.parseInt(st.nextToken());
+                long v = Long.parseLong(st.nextToken());
+                update(1, 0, N-1, i-1, v);
+            }
+        }
+
+        System.out.print(sb);
+        br.close();
+    }
+
+    static void init(int node, int start, int end){
+        if(start==end) {
+            segTree[node] = new Node(arr[start], start+1);
+//            System.out.println("node : " + node + " val : " + segTree[node].val + " idx : " + segTree[node].idx);
+        }
+        else {
+            init(node*2, start, (start+end)/2);
+            init(node*2+1, (start+end)/2 + 1, end);
+            updateMinIndex(node);
+        }
+    }
+
+    static void update(int node, int start, int end, int index, long val){
+        if(index < start || end < index) return;
+        // leaf node
+        if(start==end){
+            arr[index] = val;
+            segTree[node] = new Node(val, start+1);
+            return;
+        }
+
+        // find leaf node
+        update(node*2, start, (start+end)/2, index, val);
+        update(node*2+1, (start+end)/2+1, end, index, val);
+        updateMinIndex(node);
+    }
+
+    static Node query(int node, int start, int end, int left, int right){
+        if(right < start || end < left) return new Node(Long.MAX_VALUE, Integer.MAX_VALUE);
+
+        if(left <= start && end <= right){
+            return segTree[node];
+        }
+
+        Node lNode = query(node*2, start, (start+end)/2 , left, right);
+        Node rNode = query(node*2+1, (start+end)/2+1, end, left, right);
+        if(lNode.val == rNode.val){
+            if(lNode.idx < rNode.idx)   return lNode;
+            else                        return rNode;
+        }
+        else if (lNode.val < rNode.val) return lNode;
+        else                            return rNode;
+    }
+
+    static void updateMinIndex(int node){
+        if(segTree[node*2].val == segTree[node*2+1].val){
+            segTree[node] = new Node(segTree[node*2].val, Math.min(segTree[node*2].idx, segTree[node*2+1].idx));
+        }
+        else if(segTree[node*2].val > segTree[node*2+1].val){
+            segTree[node] = new Node(segTree[node*2+1].val, segTree[node*2+1].idx);
+        }
+        else{
+            segTree[node] = new Node(segTree[node*2].val, segTree[node*2].idx);
+        }
+    }
+}
