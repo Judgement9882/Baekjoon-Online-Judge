3f32aae468558acc6c14d84901b0a3994f5672b1
Judgement <79979086+Judgement9882@users.noreply.github.com>
2024-03-19 19:14:23 +0900

    [Silver III] Title: N과 M (1), Time: 164 ms, Memory: 20364 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+    static int N, M, C;
+    static int[] arr, selected;
+    static boolean[] vis;
+    static StringBuilder sb = new StringBuilder();
+    static void perm(int cnt){
+        if(cnt == M){
+            for(int j = 0; j < M; j++){
+                sb.append(selected[j]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = 0; i < N; i++){
+            if(vis[i]) continue;
+            vis[i] = true;
+            selected[cnt] = arr[i];
+            perm(cnt+1);
+            vis[i] = false;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        for(int i = 0; i < N; i++) arr[i] = i+1;
+        selected = new int[M];
+        vis = new boolean[N];
+        perm(0);
+        System.out.println(sb);
+        br.close();
+    }
+}
