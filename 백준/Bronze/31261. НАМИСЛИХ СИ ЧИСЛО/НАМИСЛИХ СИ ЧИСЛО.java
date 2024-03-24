4e07ca7b082416e265b81e33a7426f9f9b251ded
Judgement <79979086+Judgement9882@users.noreply.github.com>
2024-03-24 18:54:22 +0900

    [Bronze IV] Title: НАМИСЛИХ СИ ЧИСЛО, Time: 80 ms, Memory: 11432 KB -BaekjoonHub

---
+++ 백준/Bronze/31261. НАМИСЛИХ СИ ЧИСЛО/НАМИСЛИХ СИ ЧИСЛО.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        sb.append(((B+A)*A+A)*A);
+        System.out.println(sb);
+        br.close();
+    }
+}
