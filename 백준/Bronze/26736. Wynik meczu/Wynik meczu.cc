e49f77f97f14bbc83af4621c2614eb498ca29f33
Judgement <79979086+Judgement9882@users.noreply.github.com>
2024-06-01 16:29:18 +0900

    [Bronze IV] Title: Wynik meczu, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

---
+++ 백준/Bronze/26736. Wynik meczu/Wynik meczu.cc
@@ -0,0 +1,15 @@
+// https://blog.encrypted.gg/499 참고
+#include <iostream>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string str; cin >> str;
+    int A = 0, B = 0;
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == 'A') A++;
+        else B++;
+    }
+    cout << A << " : " << B;
+    return 0;
+}
