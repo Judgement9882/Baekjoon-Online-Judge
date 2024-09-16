b28977c582535725c7cded3ceb5f896e22baa9a7
Judgement <79979086+Judgement9882@users.noreply.github.com>
2024-09-16 12:34:35 +0900

    [Silver IV] Title: Card Divisibility, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/24924. Card Divisibility/Card Divisibility.cc
+++ 백준/Silver/24924. Card Divisibility/Card Divisibility.cc
@@ -5,31 +5,18 @@ using namespace std;
 long long L, R;
 
 int mod9(long long n){
-    if (n == 0) return 0;
-
+    if(n==0) return 0;
     int sum = 0;
-    long long factor = 1;
-    
-    while (n / factor > 0) {
-        long long high = n / (factor * 10);
-        long long current_digit = (n / factor) % 10;
-        long long low = n % factor;
+    for(long long i = 1; i <= n; i*=10){
+        long long high = n / (i*10);
+        long long current_digit = (n/i) % 10;
+        long long low = n%i;
 
-        // 상위 자릿수에서 1부터 9까지 반복되는 패턴을 이용해 빠르게 합을 계산
-        sum += (high * 45) % 9;  // 0~9의 자리수 합은 45
-        sum %= 9;
+        sum += (high * 45 * (i%9))%9;
 
-        // 현재 자릿수의 숫자에 따른 처리
-        sum += (current_digit * (current_digit - 1) / 2) % 9;
-        sum %= 9;
-
-        // 남은 낮은 자릿수에 대해 처리
-        sum += (current_digit * (low + 1)) % 9;
-        sum %= 9;
-
-        factor *= 10;
+        sum += ((current_digit * (current_digit - 1) / 2) * (i % 9))%9;
+        sum += (current_digit * ((low + 1) % 9))%9;
     }
-
     return sum % 9;
 }
 
