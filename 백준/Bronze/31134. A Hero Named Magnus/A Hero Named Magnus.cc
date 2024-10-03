c14b72c399e48cb634df7703df8f19fd4d55b0af
Judgement <79979086+Judgement9882@users.noreply.github.com>
2024-10-03 15:30:36 +0900

    [Bronze IV] Title: A Hero Named Magnus, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31134. A Hero Named Magnus/A Hero Named Magnus.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+	int T; cin >> T;
+	while (T--) {
+		long long x; cin >> x;
+		cout << 2 * x - 1<< "\n";
+	}
+	return 0;
+}
